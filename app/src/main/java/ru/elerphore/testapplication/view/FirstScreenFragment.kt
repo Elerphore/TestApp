ab361e27ed61722dbaa11c9b6b7c60254efd5455
Elerphore <Elerphore@gmail.com>
2023-01-31 20:08:54 +0500

    *

--- app/src/main/java/ru/elerphore/testapplication/view/FirstScreenFragment.kt
+++ app/src/main/java/ru/elerphore/testapplication/view/FirstScreenFragment.kt
@@ -1,33 +1,23 @@
 package ru.elerphore.testapplication.view
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import ru.elerphore.testapplication.R
 import ru.elerphore.testapplication.databinding.FirstScreenBinding
-import ru.elerphore.testapplication.databinding.FragmentFirstBinding
 
 class FirstScreenFragment : Fragment(R.layout.first_screen) {
 
-    private var firstScreenFragment: FirstScreenBinding? = null
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        firstScreenFragment = FirstScreenBinding.inflate(inflater, container, false)
-        return firstScreenFragment!!.root
-    }
+    private lateinit var firstScreenFragment: FirstScreenBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        firstScreenFragment!!.goButton.setOnClickListener {
-            Toast.makeText(requireContext(), "Hello", Toast.LENGTH_SHORT).show()
+        firstScreenFragment = FirstScreenBinding.bind(view)
+
+        firstScreenFragment.goButton.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstScreen_to_SecondScreen)
         }
     }
 }
