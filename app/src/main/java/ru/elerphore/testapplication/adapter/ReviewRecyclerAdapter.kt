3f803ebf7d9df519a79f17ae71d80261d7c9b30c
Elerphore <Elerphore@gmail.com>
2023-02-04 17:41:01 +0500

    *

--- app/src/main/java/ru/elerphore/testapplication/adapter/ReviewRecyclerAdapter.kt
+++ app/src/main/java/ru/elerphore/testapplication/adapter/ReviewRecyclerAdapter.kt
@@ -1,24 +1,40 @@
 package ru.elerphore.testapplication.adapter
 
+import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import ru.elerphore.testapplication.R
+import ru.elerphore.testapplication.api.dto.ReviewEntity
 
-class ReviewRecyclerAdapter : RecyclerView.Adapter<ReviewRecyclerAdapter.Holder>() {
+class ReviewRecyclerAdapter(val context: Context) : RecyclerView.Adapter<ReviewRecyclerAdapter.Holder>() {
 
-    class Holder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
+    private val reviews: MutableList<ReviewEntity> = mutableListOf()
 
+    class Holder(itemView: View?) : RecyclerView.ViewHolder(itemView!!) {
+        val image: ImageView? = null
+        val title: TextView? = null
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.review_item,parent,false)
+        return Holder(view)
     }
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
-        TODO("Not yet implemented")
+        holder.title!!.text = reviews[position].title
+        Glide.with(context).load(reviews[position].image).apply(RequestOptions().centerCrop()).into(holder.image!!)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+    fun setReview(reviews: List<ReviewEntity>) {
+        this.reviews.clear()
+        this.reviews.addAll(reviews)
     }
+
+    override fun getItemCount(): Int = reviews.size
 }
