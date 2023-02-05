aa3f0a60a16c2878d251f17b7a8691402108c020
Elerphore <Elerphore@gmail.com>
2023-02-05 19:55:58 +0500

    *

--- app/src/main/java/ru/elerphore/testapplication/api/dto/ReviewEntity.kt
+++ app/src/main/java/ru/elerphore/testapplication/api/dto/ReviewEntity.kt
@@ -1,14 +1,11 @@
 package ru.elerphore.testapplication.api.dto
 
-import android.widget.ImageView
 import ru.elerphore.testapplication.db.entity.ReviewDBEntity
 
 data class ReviewEntity(
     val image: String,
 
     val title: String,
-
-    var imageView: ImageView? = null
 )
 
 fun ReviewEntity.dbEntity() = ReviewDBEntity(image = image, title = title)
